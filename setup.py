32edcd5388bafb50011a80903abaa91672039093
Vahid Bokharaie <vahid.bokharaie@tuebingen.mpg.de>
2020-12-04 17:25:57 +0100

    v0.1.8

--- setup.py
+++ setup.py
@@ -44,5 +44,6 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
+	"Programming Language :: Python :: 3.8",
     ],
 )
